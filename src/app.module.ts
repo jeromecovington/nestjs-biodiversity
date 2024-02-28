8dbd110c167e487923f119448375adcb9de25336
Jerome Covington <jeromecovington@gmail.com>
2024-02-28 11:17:08 -0500

    feat: import biodiversity module in app

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,26 @@
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { GraphQLModule } from '@nestjs/graphql';
+import { DirectiveLocation, GraphQLDirective } from 'graphql';
+
 import { BiodiversityModule } from './biodiversity/biodiversity.module';
 
 @Module({
-  imports: [BiodiversityModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    BiodiversityModule,
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: 'schema.gql',
+      installSubscriptionHandlers: true,
+      buildSchemaOptions: {
+        directives: [
+          new GraphQLDirective({
+            name: 'upper',
+            locations: [DirectiveLocation.FIELD_DEFINITION],
+          }),
+        ],
+      },
+    }),
+  ],
 })
 export class AppModule {}
