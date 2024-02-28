06df85129581cac1920eb2877b1b012658729083
Jerome Covington <jeromecovington@gmail.com>
2024-02-28 14:25:33 -0500

    feat: describe entity

--- src/biodiversity/entities/biodiversity.entity.ts
+++ src/biodiversity/entities/biodiversity.entity.ts
@@ -1,7 +1,40 @@
-import { ObjectType, Field, Int } from '@nestjs/graphql';
+import { ObjectType, Field } from '@nestjs/graphql';
 
 @ObjectType()
 export class Biodiversity {
-  @Field(() => Int, { description: 'Example field (placeholder)' })
-  exampleField: number;
+  @Field()
+  category: string;
+
+  @Field()
+  common_name: string;
+
+  @Field()
+  county: string;
+
+  @Field()
+  distribution_status: string;
+
+  @Field()
+  federal_listing_status: string;
+
+  @Field()
+  global_conservation_rank: string;
+
+  @Field()
+  ny_listing_status: string;
+
+  @Field()
+  scientific_name: string;
+
+  @Field()
+  state_conservation_rank: string;
+
+  @Field()
+  taxonomic_group: string;
+
+  @Field()
+  taxonomic_subgroup: string;
+
+  @Field()
+  year_last_documented: string;
 }
