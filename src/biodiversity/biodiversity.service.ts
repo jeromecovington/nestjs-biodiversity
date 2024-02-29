cde15c387a8ebae8f3d3190b229d79ac91e1f69e
Jerome Covington <jeromecovington@gmail.com>
2024-02-29 14:18:26 -0500

    fix: make import path compatible with jest

--- src/biodiversity/biodiversity.service.ts
+++ src/biodiversity/biodiversity.service.ts
@@ -2,7 +2,7 @@ import { readFile } from 'node:fs/promises';
 import { resolve } from 'node:path';
 
 import { Injectable } from '@nestjs/common';
-import { findAll, findOne } from 'src/util/db';
+import { findAll, findOne } from '../util/db';
 
 type DataRecord = Record<string, string>;
 
